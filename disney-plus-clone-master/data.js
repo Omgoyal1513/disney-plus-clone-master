39fcf3b06cec81c8ff9b9a8f7ea08ddfa905c92a
om goyal <97331629+Omgoyal1513@users.noreply.github.com>
2023-09-07 22:05:19 +0530

    Add files via upload

---
+++ disney-plus-clone-master/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Following the events of “Avengers: Endgame,” Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Loki is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'With warring factions, distrust, and the unstoppable menace of the Druun wreaking havoc on Kumandra, Raya, the sword-wielding Princess of Heart and fearless Guardian of the Dragon Gem, embarks on a peril-laden quest to track down a benevolent mythical being that can heal the land.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'A young sea monster boy with the ability to assume human form while on land, who explores the town of Portorosso with his new best friends, Alberto Scorfano and Giulia Marcovaldo, experiencing a life-changing summer adventure.',
+        image: 'images/slider 5.PNG'
+    }
+]
